a81fefd79b0e2996cedd4ffba0f06d1a5210ed54
lisavanmansom <lisavanmansom@gmail.com>
2024-11-14 13:25:32 +0100

    form-task

---
+++ my-app/src/routes/form-intro-1/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import ViewTransition from '../navigation.svelte'
+    import {ArrowR, ArrowL} from '$lib'
+   </script>
+
+  <div class="app">
+    <ViewTransition />
+  </div>
+  
+  <main>
+
+    <h1>Vragenlijst gepersonaliseerd programma</h1>
+
+    <div aria-busy="true" aria-describedby="progress-bar"></div>
+    <progress value="60" max="100"></progress>
+
+    <h2>We stellen 12 vragen om een programma op maat voor je samen te stellen.</h2>
+
+    <p>*Mocht je een vraag niet prettig vinden om te beantwoorden dan kan je deze altijd overslaan. </p>
+
+    <a href="./introduction-h-2"><ArrowR /></a>
+  </main>
+  
+  <style>
+     
+  </style>
