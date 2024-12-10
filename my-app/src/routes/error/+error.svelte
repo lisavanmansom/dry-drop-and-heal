7ebc2621f3f84d99b66100bc4be4e2e9a6c4a73a
lisavanmansom <lisavanmansom@gmail.com>
2024-12-10 11:34:02 +0100

    setup errrorpage html

---
+++ my-app/src/routes/error/+error.svelte
@@ -0,0 +1,25 @@
+<script></script>
+
+<main>
+    <section id="1">
+        <h2>Error</h2>
+        <article>
+            <h3>oops... you've reached the error page</h3>
+            <p>to return to the site, go through the forest and do no turn left</p>
+            <a href="./multiple-task-1"><p>or click here</p></a>
+            <a href="#2"><span>placeholder</span></a>
+        </article>
+    </section>
+
+    <section id="2">
+        <h2>Error</h2>
+        <h3>oops... you've reached the error page</h3>
+        <a href="#3"><span>placeholder</span></a>
+    </section>
+
+    <section id="3">
+        <h2>Error</h2>
+        <h3>oops... you've reached the error page</h3>
+        <a href="#1"><span>placeholder</span></a>
+    </section>
+</main>
