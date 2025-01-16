046fbef6ed8e48bc44f5a0e32f7d5b65fd17e333
lisavanmansom <lisavanmansom@gmail.com>
2025-01-16 22:48:28 +0100

    vercel error update

--- my-app/src/routes/introductie-hulp/+page.svelte
+++ my-app/src/routes/introductie-hulp/+page.svelte
@@ -1,7 +1,6 @@
 <script>
-    import BlurgradPink from '$lib/blurgrad-Pink.svelte';
+    import BlurgradPink from '$lib/blurgrad-pink.svelte';
     import HeaderIntro from '$lib/HeaderIntro.svelte'
-  
   </script>
 
 
